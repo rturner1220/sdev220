e7840be6cfd37f5102b27e0a8e024eda4c3b1649
Rosa Turner <rmturner1220@gmail.com>
2025-04-18 11:44:06 -0400

    first test

---
+++ module05/fisrt_test/test.py
@@ -0,0 +1,17 @@
+import unittest
+from fractions import Fraction
+from my_sum import my_sum
+
+class TestSum(unittest.TestCase):
+    def test_list_int(self):
+        data = [1, 2, 3]
+        result = my_sum(data)
+        self.assertEqual(result, 6)
+
+    def test_list_fraction(self):
+        data = [Fraction(1,4), Fraction(1,4), Fraction(2,5)]
+        result = my_sum(data)
+        self.assertEqual(result, 1)
+
+if __name__ == '__main__':
+    unittest.main()
